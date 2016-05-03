aa8bb93832f5515481c2f79f2eac2e15eb85393f
MaMazav <idanigra@hotmail.com>
2016-05-03 22:03:30 +0300

    First working version of ImageryProvider for Cesium

---
+++ imagedecoderframework.js/scripts/cesiumimagedecoderimageryproviderdemo.js
@@ -0,0 +1,16 @@
+var rectangle = Cesium.Rectangle.fromDegrees(-2.0, -1.0, 2.0, 1.0);
+
+var imageryProvider = new imageDecoderFramework.ImageDecoderImageryProvider('DemoImageImplementation', {
+    url: 'dummyUrl', // this argument is passed to DemoFetchClient.openInternal() function
+    rectangle: rectangle
+});
+
+imageryProvider.setExceptionCallback(console.log);
+
+var cesiumImageryProviderViewer = new Cesium.Viewer('cesiumImageryProviderContainer');
+imageryProvider.open(cesiumImageryProviderViewer);
+cesiumImageryProviderViewer.scene.imageryLayers.addImageryProvider(imageryProvider);
+
+setTimeout(function delayedViewRectangle() {
+	cesiumImageryProviderViewer.scene.camera.setView({destination: rectangle});
+}, 100);
---
+++ imagedecoderframework.js/scripts/cesiumimagedecoderlayermanagerdemo.js
@@ -0,0 +1,14 @@
+var rectangle = Cesium.Rectangle.fromDegrees(-2.0, -1.0, 2.0, 1.0);
+
+var imageDecoderLayerManager = new imageDecoderFramework.CesiumImageDecoderLayerManager('DemoImageImplementation', {
+    url: 'dummyUrl', // this argument is passed to DemoFetchClient.openInternal() function
+    rectangle: rectangle
+});
+
+imageDecoderLayerManager.setExceptionCallback(console.log);
+
+var cesiumLayerManagerViewer = new Cesium.Viewer('cesiumLayerManagerContainer');
+imageDecoderLayerManager.open(cesiumLayerManagerViewer);
+setTimeout(function delayedViewRectangle() {
+	cesiumLayerManagerViewer.scene.camera.setView({destination: rectangle});
+}, 100);
