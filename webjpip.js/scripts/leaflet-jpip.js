2b90cebfdcd03a575d603d119e6e634daf289a8b
MaMazav <idanigra@hotmail.com>
2016-07-01 18:03:22 +0300

    First shown pixels with image-decoder-framework.js (still have exceptions when zooming in), and two bug fixes in image-decoder-framework.js

--- webjpip.js/scripts/leaflet-jpip.js
+++ webjpip.js/scripts/leaflet-jpip.js
@@ -2,10 +2,12 @@ var latLngBounds = L.latLngBounds(L.latLng(-1.0, -2.0), L.latLng(1.0, 2.0));
 
 var image = new imageDecoderFramework.ImageDecoderRegionLayer({
     imageImplementationClassName: 'webjpip.JpipImageImplementation',
-    url: 'http://kdu-server:8080/D:%5Cweb%5Cjpeg2000tries%5Ccabs.j2k',
+    url: 'http://kdu-server/path',
     latLngBounds: latLngBounds});
 
-image.setExceptionCallback(console.log);
+image.setExceptionCallback(function(exception) {
+    console.log(exception);
+});
 
 var map = L.map('leafletContainer');
 map.addLayer(image);
