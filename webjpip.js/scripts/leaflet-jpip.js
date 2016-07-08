f07877977985152bd6cf7c29f20b0717184e4c39
MaMazav <idanigra@hotmail.com>
2016-07-08 15:08:33 +0300

    Fixes before first release

--- webjpip.js/scripts/leaflet-jpip.js
+++ webjpip.js/scripts/leaflet-jpip.js
@@ -1,14 +1,29 @@
-var latLngBounds = L.latLngBounds(L.latLng(-1.0, -2.0), L.latLng(1.0, 2.0));
-
-var image = new imageDecoderFramework.ImageDecoderRegionLayer({
-    imageImplementationClassName: 'webjpip.JpipImageImplementation',
-    url: 'http://kdu-server/path',
-    latLngBounds: latLngBounds});
-
-image.setExceptionCallback(function(exception) {
-    console.log(exception);
-});
-
 var map = L.map('leafletContainer');
-map.addLayer(image);
-map.fitBounds(latLngBounds);
+var oldImage = null;
+
+function showImage() {
+    var url = document.getElementById('txtUrl').value;
+    var latLngBounds = L.latLngBounds(L.latLng(-1.0, -2.0), L.latLng(1.0, 2.0));
+
+    var image = new imageDecoderFramework.ImageDecoderRegionLayer({
+        imageImplementationClassName: 'webjpip.JpipImageImplementation',
+        url: url,
+        latLngBounds: latLngBounds});
+
+    image.setExceptionCallback(function(exception) {
+        console.log(exception);
+
+        // This message is only for demo purpose, you better use a non kdu_server to avoid this annoying bug...
+        if (exception.indexOf('Cannot extract cid from cnew response') > 0) {
+            alert('Jpip exception: ' + exception + '. That may be a kdu_server preview version bug, please restart the server');
+        }
+    });
+
+    if (oldImage !== null) {
+        map.removeLayer(oldImage);
+        oldImage = null;
+    }
+    map.addLayer(image);
+    map.fitBounds(latLngBounds);
+    oldImage = image;
+}
